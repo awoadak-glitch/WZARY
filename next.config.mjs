4a0791029f4bf7331bea21c0c6f18a8967a9d5f6
awoadak-glitch <awoadak@gmail.com>
2026-05-25 05:55:08 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/WZARY', // أضفنا اسم مستودعك هنا ليصبح الرابط متوافقاً
   images: {
     unoptimized: true,
   },
