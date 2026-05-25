8d77e10d1a296b683e39edbec6a40f8290e750a8
awoadak-glitch <awoadak@gmail.com>
2026-05-25 06:00:01 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // جيت هاب يحتاج معرفة اسم المستودع لجلب الملفات بشكل صحيح
+  basePath: '/WZARY',
+  assetPrefix: '/WZARY/',
   images: {
     unoptimized: true,
   },
